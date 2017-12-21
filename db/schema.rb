2a00fcb0ca0d164f122ba85b24f83cadb2c9d0f3
Himanshu <himanshu.mishra@vinsol.com>
2017-12-21 16:03:31 +0530

    admin rake task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219122904) do
+ActiveRecord::Schema.define(version: 20171221091945) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "email_confirmed", default: true
+    t.string "reset_digest"
+    t.datetime "resent_sent_at"
+    t.string "remember_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
