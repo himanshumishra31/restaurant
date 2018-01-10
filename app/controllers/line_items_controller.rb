76084c6d01e3e7b8736156d951b30e1dde01f9da
Himanshu <himanshumishra3@gmail.com>
2018-01-10 17:19:28 +0530

    method corrected

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -37,20 +37,16 @@ class LineItemsController < ApplicationController
     end
 
     def sufficient_stock
-      @cart.line_items.each do |line_item|
-        cart_meal = Meal.find_by(id: line_item.meal_id)
-        cart_meal.meal_items.each do |meal_item|
-          ingredient_used = meal_item.quantity * line_item.quantity
-          branch_ingredient = @branch.inventories.find_by(ingredient_id: meal_item.ingredient_id).quantity
-          ingredient_required = @meal.meal_items.find_by(ingredient_id: meal_item.ingredient_id)
-          if ingredient_required
-            ingredient_required_quantity = ingredient_required.quantity
-            if ingredient_required_quantity > branch_ingredient - ingredient_used
-              @cart.line_items.delete(@line_item) if @line_item.quantity.eql? 0
-              return false
-            end
-          end
+      @meal.meal_items.each do |meal_item|
+        ingredient_quantity_required = meal_item.quantity
+        ingredient_quantity_used = 0
+        @cart.line_items.each do |line_item|
+          cart_meal = Meal.find_by(id: line_item.meal_id)
+          ingredient = cart_meal.meal_items.find_by(ingredient_id: meal_item.ingredient_id)
+          ingredient_quantity_used += line_item.quantity * ingredient.quantity if ingredient
         end
+        branch_ingredient_quantity = @branch.inventories.find_by(ingredient_id: meal_item.ingredient_id).quantity
+        return false if ingredient_quantity_required > branch_ingredient_quantity - ingredient_quantity_used
       end
       true
     end
