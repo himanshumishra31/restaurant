77fd4bd1ddcd085b4018bb78f0f522b04c165166
Himanshu <himanshumishra3@gmail.com>
2018-01-18 19:21:45 +0530

    seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-Branch.create!(name: 'Patel Nagar', opening_time: '10:00:00', closing_time: '22:00:00', default_res: true)
+Branch.find_or_create_by(name: 'Patel Nagar', opening_time: '10:00:00', closing_time: '22:00:00', default_res: true)
